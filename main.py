db78f269aeb5dc2c5729976e4886e7f16c6959b6
YounesseAmhend <younesseamhend2019@gmail.com>
2023-02-20 21:29:31 +0000

    add cleaning function

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
 import time
-df = pd.read_csv("Gulf.csv", low_memory=False,usecols= [''])
-profile = ProfileReport(df)
-profile.to_file("Analysis.html")
-profile.to_file("Analysis.json")
-print("\n\n time :",time.process_time(), "s")
-
 
 def cleanSpecial(df):
     # supprimer les caractères speciaux (*&^£@~#!*^%") dans les columns donnees
@@ -14,4 +8,12 @@ def cleanSpecial(df):
         df[column] = df[column].str.replace(r"\W", "")
     # cree nouveau fichier pour afficher les resultas
     df.to_csv("results.csv")
+    
+df = pd.read_csv("Gulf.csv", low_memory=False,usecols= ['Hospital'])
+profile = ProfileReport(df)
+profile.to_file("Analysis.html")
+profile.to_file("Analysis.json")
+# cleanSpecial(df) test
+print("\n\n time :",time.process_time(), "s")
+
     
