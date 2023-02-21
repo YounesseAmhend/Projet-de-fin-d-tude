7b88f59650b220d2a72a400169f436bffa8b29dd
YounesseAmhend <younesseamhend2019@gmail.com>
2023-02-21 17:33:24 +0000

    voir

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 import time
 
 def cleanSpecial(df):
+    
     # supprimer les caractères speciaux (*&^£@~#!*^%") dans les columns donnees
     #for column in df.columns:
     #    df[column] = df[column].str.replace(r"\W", "")
     # cree nouveau fichier pour afficher les resultas
     df.to_excel("results.xlsx")
 
+to_numeric = [
+              'Gender', 'Education', 'Work', 'Cardiac_Arrest_Admission', 'Non_Cardiac_Condition',
+              'Hypertension', 'Dyslipidemia', 'DM', 'DM_Type', 'DM_Treatment', 'Smoking_History',
+              'Lipid_24_Collected'
+              ]
 variables = [
              "Gender", "Age", "Education", "Work", "Cardiac_Arrest_Admission", "Non_Cardiac_Condition",
              "Hypertension" ,  "Dyslipidemia" , "DM" , "Year_DM_Diagnosed" , "DM_Duration" , "DM_Type",
@@ -17,13 +25,39 @@ variables = [
              "Creatinine_Clearance", "Heart_Rate"
              ]
 
-df = pd.read_csv("Gulf.csv", low_memory=False, usecols=variables)
-profile = ProfileReport(df)
+variables_not_used = []
 
+df = pd.read_csv("Gulf.csv", low_memory=False, usecols=variables)
+
+
+#profile = ProfileReport(df)
+# profile.to_file("Analysis.html")
+# profile.to_file("Analysis.json")
+# cleanSpecial(df)
+
+# for var in to_numeric:
+#     print("\n\n\n\n"+var, ":   ")
+#     for l in df[var].unique():
+#         print(l)
+        
+ohe = OneHotEncoder()
+
+feature_array = ohe.fit_transform(df[to_numeric]).toarray()
+feature_labels = ohe.categories_
+feature_labels = np.concatenate(feature_labels, dtype=object)
+
+
+
+
+dp = pd.DataFrame(feature_array,columns=feature_labels)
+
+dl = df.merge(dp,left_index=True, right_index=True)
+dl.to_excel("results.xlsx")
+
+profile = ProfileReport(dl)
 profile.to_file("Analysis.html")
 profile.to_file("Analysis.json")
 
-cleanSpecial(df)
 print("\n\n time :",time.process_time(), "s")
 
     
