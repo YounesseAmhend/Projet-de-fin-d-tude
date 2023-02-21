d372ba47c7254cd4e8b7cc3c4e26ae3fb0f664a1
YounesseAmhend <younesseamhend2019@gmail.com>
2023-02-21 20:15:29 +0000

    voir

--- main.py
+++ main.py
@@ -4,19 +4,13 @@ from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import time
 
-def cleanSpecial(df):
-    
-    # supprimer les caractères speciaux (*&^£@~#!*^%") dans les columns donnees
-    #for column in df.columns:
-    #    df[column] = df[column].str.replace(r"\W", "")
-    # cree nouveau fichier pour afficher les resultas
-    df.to_excel("results.xlsx")
 
 to_numeric = [
               'Gender', 'Education', 'Work', 'Cardiac_Arrest_Admission', 'Non_Cardiac_Condition',
               'Hypertension', 'Dyslipidemia', 'DM', 'DM_Type', 'DM_Treatment', 'Smoking_History',
               'Lipid_24_Collected'
               ]
+
 variables = [
              "Gender", "Age", "Education", "Work", "Cardiac_Arrest_Admission", "Non_Cardiac_Condition",
              "Hypertension" ,  "Dyslipidemia" , "DM" , "Year_DM_Diagnosed" , "DM_Duration" , "DM_Type",
@@ -25,27 +19,20 @@ variables = [
              "Creatinine_Clearance", "Heart_Rate"
              ]
 
-variables_not_used = []
+numeric_variables = ['Age', 'Year_DM_Diagnosed', 'DM_Duration', 'Waist', 'BMI', 'Fasting_Blood_Glucose_Value_SI_Units',
+                     'HbA1C_Admission_Value', 'Cholesterol_Value_SI_Units', 'Triglycerides_Value_SI_Units', 
+                     'Creatinine_Clearance', 'Heart_Rate'
+                     ]
 
-df = pd.read_csv("Gulf.csv", low_memory=False, usecols=variables)
-
-
-#profile = ProfileReport(df)
-# profile.to_file("Analysis.html")
-# profile.to_file("Analysis.json")
-# cleanSpecial(df)
-
-# for var in to_numeric:
-#     print("\n\n\n\n"+var, ":   ")
-#     for l in df[var].unique():
-#         print(l)
+df = pd.read_csv("Gulf.csv", low_memory=False, usecols=numeric_variables)
+ds = pd.read_csv("Gulf.csv", low_memory=False, usecols=to_numeric)
+    
         
 ohe = OneHotEncoder()
 
-feature_array = ohe.fit_transform(df[to_numeric]).toarray()
+feature_array = ohe.fit_transform(ds[to_numeric]).toarray()
 feature_labels = ohe.categories_
 feature_labels = np.concatenate(feature_labels, dtype=object)
-
 
 
 
@@ -54,9 +41,9 @@ dp = pd.DataFrame(feature_array,columns=feature_labels)
 dl = df.merge(dp,left_index=True, right_index=True)
 dl.to_excel("results.xlsx")
 
-profile = ProfileReport(dl)
-profile.to_file("Analysis.html")
-profile.to_file("Analysis.json")
+# profile = ProfileReport(dl)
+# profile.to_file("Analysis.html")
+# profile.to_file("Analysis.json")
 
 print("\n\n time :",time.process_time(), "s")
 
