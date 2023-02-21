38787e60c885ad974cf899dbbbc6e124537c9a9d
YounesseAmhend <younesseamhend2019@gmail.com>
2023-02-21 14:12:06 +0000

    voir resultas.xlsx

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pandas as pd
-from pandas_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 import time
 
 def cleanSpecial(df):
     # supprimer les caractères speciaux (*&^£@~#!*^%") dans les columns donnees
-    for column in df.columns:
-        df[column] = df[column].str.replace(r"\W", "")
+    #for column in df.columns:
+    #    df[column] = df[column].str.replace(r"\W", "")
     # cree nouveau fichier pour afficher les resultas
-    df.to_csv("results.csv")
-    
-df = pd.read_csv("Gulf.csv", low_memory=False,usecols= ['Hospital'])
+    df.to_excel("results.xlsx")
+
+variables = [
+             "Gender", "Age", "Education", "Work", "Cardiac_Arrest_Admission", "Non_Cardiac_Condition",
+             "Hypertension" ,  "Dyslipidemia" , "DM" , "Year_DM_Diagnosed" , "DM_Duration" , "DM_Type",
+             "DM_Treatment" , "Smoking_History" , "Waist" , "BMI"  , "Fasting_Blood_Glucose_Value_SI_Units",
+             "HbA1C_Admission_Value" , "Lipid_24_Collected" , "Cholesterol_Value_SI_Units" , "Triglycerides_Value_SI_Units",
+             "Creatinine_Clearance", "Heart_Rate"
+             ]
+
+df = pd.read_csv("Gulf.csv", low_memory=False, usecols=variables)
 profile = ProfileReport(df)
+
 profile.to_file("Analysis.html")
 profile.to_file("Analysis.json")
-# cleanSpecial(df) test
+
+cleanSpecial(df)
 print("\n\n time :",time.process_time(), "s")
 
     
