f34edde07a9f44b798e9ce0a224da4ac9511369b
YounesseAmhend <younesseamhend2019@gmail.com>
2023-02-28 17:27:43 +0000

    cleaned database

--- main.py
+++ main.py
@@ -25,11 +25,13 @@ numeric_variables = ['Age', 'Year_DM_Diagnosed', 'DM_Duration', 'Waist', 'BMI', 
 # cleaning functions
 def clean(df, column:str, min:float, max:float, conv:float=1.0):
     for i in range(len(df[column])):
+        if df[column][i]=="":
+            continue
         value = float(df[column][i])
         if min <= value and value <= max:
             continue
         elif  min <= (value*conv) and (value*conv )<= max:
-            df.loc[i, column]=str(value*conv)
+            df.loc[i, column]=value*conv
         else:
             df.loc[i, column]=""
 def replaceValue(df, column:str, newValue:str, valueToReplace:str):
@@ -53,10 +55,10 @@ dl = df.merge(numeric,left_index=True, right_index=True)
 dl.to_excel("results.xlsx")
 dall.to_excel("old.xlsx")
 
-profile = ProfileReport(dall)
-profile.to_file("AnalysisAll.html")
-profile = ProfileReport(dl)
-profile.to_file("encodedAnalysis.html")
+# profile = ProfileReport(dall)
+# profile.to_file("AnalysisAll.html")
+# profile = ProfileReport(dl)
+# profile.to_file("encodedAnalysis.html")
 
 # #cleaning
 clean(df=dall, column="Year_DM_Diagnosed", min=1967 ,max=2013,conv=1)
@@ -69,12 +71,27 @@ clean(df=dall, column="BMI", min=9 ,max=105,conv=1)
 clean(df=dall, column="Creatinine_Clearance", min=5 ,max=200,conv=60)
 clean(df=dall, column="Fasting_Blood_Glucose_Value_SI_Units", min=1 ,max=20,conv=0.056)
 
+clean(df=dall, column="Year_DM_Diagnosed", min=1967 ,max=2013,conv=1)
+clean(df=dall, column="DM_Duration", min=0 ,max=45,conv=1)
+clean(df=dall, column="Heart_Rate", min=25 ,max=300,conv=1)
+clean(df=dall, column="HbA1C_Admission_Value", min=3.9 ,max=14.1,conv=1)
+clean(df=dall, column="Cholesterol_Value_SI_Units", min=1 ,max=25.86,conv=0.02586)
+clean(df=dall, column="Triglycerides_Value_SI_Units", min=0.2 ,max=22.58,conv=0.01129)
+clean(df=dall, column="BMI", min=9 ,max=105,conv=1)
+clean(df=dall, column="Creatinine_Clearance", min=5 ,max=200,conv=60)
+clean(df=dall, column="Fasting_Blood_Glucose_Value_SI_Units", min=1 ,max=20,conv=0.056)
+
+dl.to_excel("resultsCleaned.xlsx")
+dall.to_excel("oldCleaned.xlsx")
+dall.to_csv("oldCleaned.csv")
+
+dall = pd.read_csv("oldCleaned.csv", low_memory=False, usecols=variables)
+
 profile = ProfileReport(dall)
 profile.to_file("AnalysisAllCleaned.html")
 profile = ProfileReport(dl)
 profile.to_file("encodedAnalysisCleaned.html")
-dl.to_excel("resultsCleaned.xlsx")
-dall.to_excel("oldCleaned.xlsx")
+
 
 print("\n\n time :",time.process_time(), "s")
 
